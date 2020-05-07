98916d3e281f80568e86e94eaedb87fbf7ee5454
Jay Kiharani <thekiharani@gmail.com>
2020-05-07 09:51:50 +0300

    added flask-moment for data formatting

--- wallet/__init__.py
+++ wallet/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_moment import Moment
 # Make use of variables in .env file within the app
 from environs import Env
 import os
@@ -16,6 +17,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Wallet.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+moment = Moment(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
